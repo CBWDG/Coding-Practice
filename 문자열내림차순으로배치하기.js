a467525c0dcb1ebaf034f75b591d4e8be28c35f3
CBWDG <zaratole@naver.com>
2023-06-29 21:42:43 +0900

    test

--- 문자열내림차순으로배치하기.js
+++
@@ -1,5 +0,0 @@
-function solution(s) {
-    let answer
-    answer = s.split('').sort().reverse().join('');
-    return answer;
-}
--- 없는숫자더하기.js
+++
@@ -1,9 +0,0 @@
-function solution(numbers) {
-    let answer = 0;
-    let result;
-    for(let i = 0; i < numbers.length; i++) {
-        answer += numbers[i]
-    }
-    result = 45 - answer;
-    return result;
-}
